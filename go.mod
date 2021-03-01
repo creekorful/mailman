256d8cac0a966d2a0ae4c6c4fa7ec3a35a5d1907
Aloïs Micard <alois@micard.lu>
2021-03-01 21:02:05 +0100

    working smtp sending

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module gihtub.com/creekorful/mailman
 
 go 1.15
 
-require github.com/emersion/go-smtp v0.14.0 // indirect
+require (
+	github.com/emersion/go-smtp v0.14.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- mailman.go
+++ mailman.go
@@ -1,18 +1,52 @@
 package main
 
 import (
-	"io/ioutil"
+	"github.com/emersion/go-sasl"
+	"github.com/emersion/go-smtp"
+	"gopkg.in/yaml.v2"
 	"log"
 	"os"
+	"path/filepath"
 )
 
+type Config struct {
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+
+	SMTPAddr string `yaml:"smtp_addr"`
+}
+
 func main() {
-	b, err := ioutil.ReadAll(os.Stdin)
+	conf, err := readConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if err := ioutil.WriteFile("/var/log/mailman.log", b, 0640); err != nil {
+	auth := sasl.NewLoginClient(conf.Username, conf.Password)
+
+	to := []string{"lunamicard@gmail.com"} // todo parse
+
+	if err := smtp.SendMail(conf.SMTPAddr, auth, conf.Username, to, os.Stdin); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func readConfig() (Config, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return Config{}, err
+	}
+
+	f, err := os.Open(filepath.Join(homeDir, ".mailman.yaml"))
+	if err != nil {
+		return Config{}, err
+	}
+	defer f.Close()
+
+	var conf Config
+	if err := yaml.NewDecoder(f).Decode(&conf); err != nil {
+		return Config{}, err
+	}
+
+	return conf, nil
 }
